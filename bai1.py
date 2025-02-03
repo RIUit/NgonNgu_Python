9fd29db3f2933865b4aa284463501f2ba5da1f84
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-03 21:40:37 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,9 @@
+# NHAP DU LIEU TREN 1 DONG
+
+#Bước 1:Nhập dữ liệuliệu
+s=input('Nhap 4 so nguyen : ')
+#Bước 2:Tách các số ra
+x=s.split()
+#Bước 3:Sử dụng map()
+a,b,c,d=map(int,x)
+print(a,b,c,d)
