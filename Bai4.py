9b502b6d26318bf40bfaad58e485e0bb580efe77
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-05 18:42:37 +0700

    Add files via upload

---
+++ Bai4.py
@@ -0,0 +1,5 @@
+#Nhap 2 so nguyen tren 1 dong
+x,y=map(int,input().split())
+
+#Output:x^y (?2 so thap phan)
+print(x**y)
---
+++ Bai5.py
@@ -0,0 +1,6 @@
+from math import *
+n=int(input())
+res1=sqrt(n)
+print('%.2f' % res1)
+res2=pow(n,1/3)
+print('%.3f' % res2)
