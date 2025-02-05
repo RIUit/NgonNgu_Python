e6f864bf13b3a859462969563ef08ceb30f1e1db
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-05 20:16:25 +0700

    Add files via upload

---
+++ Bai11.py
@@ -0,0 +1,15 @@
+from math import *
+#Nhap vao so nguyen duong
+n=int(input())
+
+#In ra:
+
+#1.Hai lan n
+print(n*2)
+print()
+#2.Muoi lan n
+print(n*10)
+#3.Chia nguyen n voi 2
+print(n//2)
+#4.n chia voi 2 lay 3 chu so thap phan
+print('%.3f' % (n/2))
