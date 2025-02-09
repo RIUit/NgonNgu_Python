23034be82561b72f8e687d8218a744572f4173a1
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-09 15:54:09 +0700

    Add files via upload

---
+++ KDL_Bai26.py
@@ -0,0 +1,9 @@
+#  Số lớn nhất trong 4 số
+'''Cho 4 số nguyên a, b, c, d. Hãy tìm số lớn nhất và nhỏ nhất trong 4 số này.      '''
+
+#Nhap vao 4 so nguyen a b c d
+a,b,c,d=map(int,input().split())
+
+#Processing va output
+print(max(a,b,c,d),min(a,b,c,d))
+
